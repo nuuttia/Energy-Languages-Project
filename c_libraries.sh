7bf68660184fcbae0de9219452c02649f5f19c91
Andre Malheiro <andre.pedro.malheiro@gmail.com>
2023-01-06 23:23:02 +0000

    C is working now

---
+++ c_libraries.sh
@@ -0,0 +1,21 @@
+# Downloading & Installing htslib.
+git clone https://github.com/samtools/htslib.git
+cd htslib/
+git submodule update --init --recursive
+autoreconf -i
+./configure
+make
+sudo make install
+cd ..
+rm -rf htslib
+
+#APR library
+curl https://dlcdn.apache.org//apr/apr-1.7.0.tar.gz --output apr.tar.gz
+tar -xf apr.tar.gz -C .
+rm apr.tar.gz
+cd apr
+./configure
+make
+sudo make install
+cd ..
+rm -rf apr
