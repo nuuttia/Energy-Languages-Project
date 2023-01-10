a7588970c9208e814b1edd6ac42fda94816bfc74
Andre Malheiro <andre.pedro.malheiro@gmail.com>
2023-01-10 01:58:48 +0000

    pascal added

--- get_language_versions.sh
+++ get_language_versions.sh
@@ -4,6 +4,9 @@ sudo tar -xf jruby.tar.gz -C /usr/local/
 rm jruby.tar.gz
 
 #Ruby 2.7.0-preview1 was incompatible with current openssl version. Therefore, we used the current version available in the package manager dnf (ruby-3.1.3)
+# Also install ruby-dev package
+#INstalling ruby gmp module
+gem install gmp
 
 #Php 1.7.4.
 curl https://www.php.net/distributions/php-7.1.4.tar.gz --output php.tar.gz
@@ -55,3 +58,24 @@ cd fastutil
 make sources
 ant jar
 cd ../../..
+
+#Haskell 8.8.1 for Fedora
+wget https://downloads.haskell.org/~ghc/8.8.1/ghc-8.8.1-x86_64-fedora27-linux.tar.xz
+sudo tar -xf ghc-8.8.1-x86_64-fedora27-linux.tar.xz
+rm ghc-8.8.1-x86_64-fedora27-linux.tar.xz
+cd ghc-8.8.1-x86_64-fedora27-linux
+./configure --prefix=/usr/local/
+sudo make install
+cd ..
+rm -rf ghc-8.8.1-x86_64-fedora27-linux
+
+## Install LLVM11 and CLANG using your packet manager
+
+# Pascal 3.0.2
+wget https://sourceforge.net/projects/freepascal/files/Linux/3.2.2/fpc-3.2.2.x86_64-linux.tar
+tar -xf fpc-3.2.2.x86_64-linux.tar
+rm fpc-3.2.2.x86_64-linux.tar
+cd fpc-3.2.2.x86_64-linux
+sudo ./install.sh
+cd ..
+rm -rf fpc-3.2.2.x86_64-linux/
